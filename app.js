0e5f5544e37aaea58782f639a39be65569e53575
Nazim Boudeffa <boudeffa@gmail.com>
2020-05-19 06:30:43 +0200

    Added Home Page

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const hommb = new Discord.Client();
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/public'))
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('index')
+})
+
+let port = 3000;
+
+app.listen(port, function () {
+  console.log('HoMMB app listening on port 3000!')
+})
 
 hommb.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
