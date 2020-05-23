4e24b18965423d035d349292cfa3e3f61221faac
Nazim Boudeffa <boudeffa@gmail.com>
2020-05-23 08:49:47 +0200

    Started to code the Game

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-require('dotenv').config();
 const Discord = require('discord.js');
-const hommb = new Discord.Client();
+
+const { CONFIG_TOKEN } = require('./config.json');
+const { greetings } = require('./data.json');
+
+const client = new Discord.Client();
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
+
+const Game = require('./game.js');
+
+var isPlaying = false;
+var voiceChannel, dispatcher;
+
+var token = CONFIG_TOKEN || process.env.TOKEN
+var port = 3000 || process.env.PORT
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
@@ -16,23 +27,136 @@ app.get('/', function (req, res) {
   res.render('index')
 })
 
-let port = process.env.PORT || 3000;
-
 app.listen(port, function () {
   console.log('HoMMB app listening on port 3000!')
 })
 
-hommb.on('ready', () => {
-  console.log(`Logged in as ${hommb.user.tag}!`);
+let playersName = []; // tracks each player that joins
+let playersSaveFile = [];
+let currPlayers = 0; //tracker for total players online
+let INIT_GAME; //Initilizes our game engine
+let userJoined = false;
+let inBattle = false;
+let session_id;
+let currentUser;
+
+/* User joins the server */
+client.on('message', (msg) => {
+    if(msg.content === '!join'){
+
+        // Prevents multiple instances of the same person from joining
+        for(var x = 0; x < playersName.length; x++){
+            if(playersName[x].playerName===msg.author.username){
+                return playersName[x].playerName;
+            }
+         }
+
+        function checkData(){
+        // Checks the object that stores players data so they can retrieve their saved progress
+        for(var s = 0; s < playersSaveFile.length; s++){
+            if(playersSaveFile[s].playerName===msg.author.username){
+                for(var y = 0; y < playersName.length; y++){
+                /* If the name in the save file object matches the name of the current user online, retrieve
+                that specific persons saved progress */
+                if(playersName[y].playerName===playersSaveFile[s].playerName){
+                msg.channel.send('This player has a saved file!');
+                /* Grab the index from the saved file and set it to the index of the current players object
+                This is where you load all the players progress(such as health,damage,id,etc) */
+                playersName[y].index=playersSaveFile[s].index;
+                msg.channel.send('Saved file has been loaded.');
+                } else{
+                   }
+                }
+              }
+           }
+        }
+
+        function init(){
+            INIT_GAME = new Game(playersName, client, 'bot-testing', currPlayers);
+            let myRet = INIT_GAME.startGame();
+            const embed = new Discord.MessageEmbed()
+            .setTitle("Welcome to Heroes of Might and Magic")
+            .setColor(0xFF0000)
+            .addField(`${msg.author.username} has Joined`, myRet);
+            msg.channel.send(embed);
+            msg.channel.send(`${msg.author} type !commands to see the list of commands.`);
+            return;
+        }
+
+        // Increasing the players online count when someone joins
+        currPlayers++;
+        userJoined = true;
+
+        // Creates a new object to store information about the player who joined
+        playersName.push({
+            "playerName": msg.author.username,
+            "index": 99,
+            "health": 150,
+            "inBattle": false
+        });
+
+        checkData();
+        init();
+    }
+
+    /* User types the fight command */
+    if(userJoined == true){
+        if(msg.content === '!fight'){
+            for(var d = 0; d < playersName.length; d++){
+                session_id = playersName[d];
+            }
+
+            console.log("You attacked monster.");
+            currentUser = msg.author.username;
+            let grabCurrPlayer = currentUser;
+            let playerStats = session_id;
+            msg.channel.send(`${INIT_GAME.initBattle(grabCurrPlayer, playerStats)}`);
+        }
+
+        else if(msg.content === '!leave'){
+            let tempLeave = msg.author.username;
+
+            for(var x = 0; x < playersName.length; x++){
+                if(playersName[x].playerName === tempLeave){
+                    playersName.splice(x, 1);
+                    currPlayers--;
+                }
+            }
+            msg.channel.send([`${tempLeave} has left the quest.`]);
+            // userJoined = false;
+        }
+
+        /* Rejuvanates players and monster hitpoints */
+        else if(msg.content === '!new'){
+            msg.channel.send(INIT_GAME.newGame(session_id));
+        }
+
+        /* Simply checks the bonus damage. command for developer*/
+        else if(msg.content === '!bonus'){
+            msg.channel.send(INIT_GAME.bonusAttack());
+        }
+
+        /* checks whose currently online. command for developer*/
+        else if(msg.content === '!online'){
+            msg.channel.send(INIT_GAME.getOnline());
+            console.log(playersName);
+        }
+
+        /* The help command */
+        else if(msg.content === '!commands'){
+          msg.channel.send("Try !fight");
+        }
+
+        if(msg.content === '!test'){
+            msg.channel.send(msg.author);
+
+            console.log(msg.author);
+        }
+    }
 });
 
-hommb.on('message', msg => {
-  if (msg.content === '!ping') {
-    msg.reply('Pong!');
-  }
-  if (msg.content === '!life') {
-    msg.reply('You have 7 lifes');
-  }
+client.on('ready', () => {
+    console.log('HoMMB is now connected to Discord');
 });
 
-hommb.login(process.env.TOKEN);
+client.login(token);
---
+++ game.js
@@ -0,0 +1,120 @@
+class Game {
+    constructor(player, client, channelName='hommb-test', playersOnline){
+         this.client = client;
+         this.channelName = channelName;
+         this.currentPlayer = player;
+         this.playersOnline = [];
+         this.hitpoints = 120;
+         this.damage = '';
+         this.chance = 3;
+         this.inBattle = false;
+         this.online = playersOnline;
+
+         this.monster = [{
+             hp: Math.floor(Math.random() * 200),
+             temphp: 0,
+             damage: 10
+         }];
+    };
+
+    /* main menu information, players online */
+    startGame(){
+            for(var x = 0; x < this.currentPlayer.length; x++){
+                this.playersOnline.push(this.currentPlayer[x]);
+                if(this.playersOnline[x] === this.currentPlayer[x]){
+                    return [`Players Online: ${this.online}\n`];
+            }
+         }
+    }
+
+    /* Battle system */
+    initBattle(currPlayer, currPlayerStats){
+        this.inBattle = true;
+        let npcHP = this.monster[0].hp;
+        let numberOfAttacks = 0;
+        let totalDamage=0, totalBonusDamage=0;
+
+        while( this.monster[0].hp > 0 ){
+            let playerDamage = Math.floor(Math.random() * (npcHP / 4));
+            if(this.bonusAttack() === 2){
+                console.log(`Bonus Attack: ${this.bonusAttack()}`);
+                console.log(`Regular damage without bonus attack: ${playerDamage}`);
+                playerDamage = playerDamage + 2;
+            }
+            this.monster[0].hp -= playerDamage;
+
+            for(var x = 0; x < this.currentPlayer.length; x++){
+                if(this.currentPlayer[x].playerName===currPlayer){
+                    this.currentPlayer[x].health -= this.monster[0].damage;
+                }
+            }
+
+            console.log('Monster: ' + this.monster[0].hp);
+            console.log('Player: ' + currPlayerStats.health);
+            console.log(`${currPlayer} has attacked for ${playerDamage}`);
+            console.log(`NPC health: ${this.monster[0].hp}`);
+
+            this.inBattle = false;
+            numberOfAttacks++;
+            totalDamage += playerDamage;
+            totalBonusDamage = playerDamage + this.bonusAttack();
+        }
+
+        for(var x = 0; x < this.currentPlayer.length; x++){
+            if(this.currentPlayer[x].playerName===currPlayer){
+            if(this.currentPlayer[x].health <= 0){
+                return [`\nYou are dead. Type !newgame to refresh your stats!`];
+            }
+          }
+        }
+
+        if(this.monster[0].hp <= 0){
+            for(var x = 0; x < this.currentPlayer.length; x++){
+                if(this.currentPlayer[x].playerName===currPlayer){
+                    let maxDamage = totalDamage + totalBonusDamage;
+                    this.monster[0].hp = Math.floor(Math.random() * 50);
+                    return [`${currPlayer} has attacked ${numberOfAttacks} times dealing ${totalDamage} + (${totalBonusDamage}) bonus damage for a total of ${maxDamage} damage. The monster is dead.\n
+                    Your Health: ${this.currentPlayer[x].health}`];
+                }
+            }
+        }
+        else{
+            return [`\nType !newgame to refresh your stats!`];
+        }
+     }
+
+    /* bonus attack damage [ 1 in 3 chance ] */
+    bonusAttack(bonusDamage){
+        let chance = Math.floor(Math.random() * 3);
+        return chance === 2 ? bonusDamage = 2 : false;
+    }
+
+    /* displays players currently online */
+    getOnline(){
+        let totalPlayers = [];
+
+        for(var x = 0; x < this.currentPlayer.length; x++){
+            if(this.currentPlayer[x].index===99 || this.currentPlayer[x].index === undefined){
+                this.currentPlayer[x].index='type !save to save your progress';
+            }
+            totalPlayers.push([`ID: [${this.currentPlayer[x].index}] [${this.currentPlayer[x].playerName}]`]);
+        }
+        console.log(totalPlayers);
+        return totalPlayers;
+    }
+
+    /* refresh stats */
+    newGame(currPlayer){
+        for(var x = 0; x < this.currentPlayer.length; x++){
+            if(this.currentPlayer[x].playerName===currPlayer){
+            if(this.currentPlayer[x].health <= 0){
+                this.monster[0].hp = Math.floor(Math.random() * 50);
+                this.currentPlayer[x].health = 150;
+                return [`You feel good as new and you can !fight again.`];
+            }
+          }
+        }
+    }
+}
+
+module.exports = Game;
