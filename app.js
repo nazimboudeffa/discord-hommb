b88d4049e220c32eb59114c88aa02b2dae8476f6
Nazim Boudeffa <boudeffa@gmail.com>
2020-05-27 11:47:56 +0200

    Centered hero in browser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const { CONFIG_TOKEN } = require('./config.json');
+//const { CONFIG_TOKEN } = require('./config.json');
 const { greetings } = require('./quest/data.json');
 
 const client = new Discord.Client();
@@ -12,8 +12,8 @@ const path = require('path')
 const Game = require('./quest/game.js');
 const Player = require('./quest/player.js');
 
-var token = CONFIG_TOKEN || process.env.TOKEN
-//var token = process.env.TOKEN
+//var token = CONFIG_TOKEN || process.env.TOKEN
+var token = process.env.TOKEN
 var port = process.env.PORT || 3000
 
 // set the view engine to ejs
